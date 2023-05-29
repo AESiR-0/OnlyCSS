4c25384c6119160598ae6bf41410ee3468ee4d4a
AESiR <cyborgkiller1008@gmail.com>
2023-05-30 03:13:36 +0530

    navbar

--- script.js
+++
@@ -1,9 +0,0 @@
-const hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
-const mobileMenuWrapper = document.getElementsByClassName('mobile-menu-wrapper')[0];
-
-hamburgerMenu.addEventListener('click', () => {
-    mobileMenuWrapper.classList.toggle('open');
-    hamburgerMenu.classList.toggle('fa-times');
-    hamburgerMenu.classList.toggle('fa-bars');
-
-})
